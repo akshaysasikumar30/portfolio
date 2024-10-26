532dacd78f866a57023dc0192974a3feb7093325
Akshay Sasikumar <akshay14145@gmail.com>
2024-10-26 22:19:15 +0530

    added-more-info

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,39 +3,32 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Vue.js, as well as back-end technologies like Node.js,PHP, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 months of hands-on experience, I have honed my skills in front-end technologies like React and Vue.js, as well as back-end technologies like Node.js,PHP, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a software developer with a background in civil engineering and a strong passion for full-stack development. During a recent 5-month internship, I gained hands-on experience with PHP, Vue.js (Vue 2), and MySQL, contributing to live projects and building my skills in both front-end and back-end development. My GitHub portfolio includes projects in Java, JavaScript, and Express, reflecting my commitment to mastering the full stack. As the saying goes, "If you do a job that you love, you'll never have to work a day in your life." This fuels my dedication and ambition to excel as a Full Stack Developer.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "May-2024 to October-2024 ",
+    role: "Full Stack Developer Intern",
+    company: "Linways Technologies Pvt Ltd.",
+    description: `Developed a social media website during the first month of my internship using PHP, Vue.js, and MySQL. In the next months, supported the production team by debugging, optimizing, and enhancing live applications, gaining real-world experience.Collaborated with the production team to debug and optimize live web applications, resulting in a 20% improvement in page load times.`,
+    technologies: ["Javascript", "Vue.js", "PHP", "MySQL"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    year: "Mar - 2023 to Dec -2023",
+    role: "Site Supervisor",
+    company: "Sadsang Engineers",
+    description: `At Kinfra IT Park and SNIMS Medical College, I oversaw daily site operations, coordinated with contractors, managed schedules, and ensured quality standards and safety protocols were met. Through these projects, I developed strong team management and leadership skills, along with hands-on problem-solving abilities essential for efficient project delivery`,
+    technologies: [],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+    year: "Aug -2022 to Feb-2023",
+    role: "Sales & Styling Professional",
+    company: "Aurelia By TCNS Clothing Pvt Ltd",
+    description: `While pursuing my Postgraduate Diploma in Kochi, I worked part-time at Aurela, where I developed excellent communication and customer service skills. This experience enhanced my active listening, time management, and adaptability, allowing me to effectively engage with diverse clients and thrive in a dynamic environment.`,
+    technologies: [],
+  }
 ];
 
 export const PROJECTS = [
@@ -51,21 +44,21 @@ export const PROJECTS = [
     image: project2,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+    technologies: ["HTML", "CSS", "Javascript"],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "Javascript", "React", "Tailwind-CSS"],
   },
   {
-    title: "Blogging Platform",
+    title: "Roll The Dice",
     image: project4,
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    technologies: ["HTML", "CSS", "Javascript"],
   },
 ];
 
